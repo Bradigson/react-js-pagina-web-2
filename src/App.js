8e205867ee72e9dce9923a6a73fb25e908d08475
Bradigson <71954800+Bradigson@users.noreply.github.com>
2022-02-17 23:38:58 -0400

    banner2 done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Header from './pagina-web-2/Header';
 import Banner1 from './pagina-web-2/Banner1';
+import Banner2 from './pagina-web-2/banner2';
 function App() {
   return (
     <div className="App">
       <Header/>
       <Banner1/>
+      <Banner2/>
     </div>
   );
 }
---
+++ src/pagina-web-2/banner2.js
@@ -0,0 +1,38 @@
+import './Banner2.css';
+import React from 'react';
+import Banner2Img from './imgs/Writing room-pana.svg';
+
+
+
+
+const Brands = (props)=>{
+    return(
+        <div className="banner2-brands mt-4 pb-4">
+            <img src={props.BlackSwan} alt=""/>
+            <img src={props.quadra} alt=""/>
+            <img src={props.essence} alt=""/>
+            <img src={props.generic} alt=""/>
+        </div>
+    )
+}
+class Banner2 extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {
+
+        }
+    }
+    render(){
+        return(
+            <div className="banner2">
+                <div className="banner-img-1">
+                    <img src={Banner2Img}alt="Banner2-img"/>
+                </div>
+                <Brands BlackSwan={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6pdpMx88iiQAFQKvbhHGJBWvRKzHE-BFgBg&usqp=CAU"} quadra={"https://qco.net/wp-content/uploads/sites/3/2020/09/quadra-logo-min.png"} essence={"https://img2.freepng.es/20180426/stw/kisspng-cosmetics-eye-shadow-cruelty-free-essence-brand-5ae21bfbb43312.2614416815247677397381.jpg"} generic={"https://t3.ftcdn.net/jpg/01/82/26/30/360_F_182263003_qetLxvmZtCA4SJ3mBragsnPLWeh1sC8Z.jpg"}/>
+            </div>
+        )
+    }
+}
+
+
+export default Banner2;
